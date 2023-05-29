eb8b4566516b42bd5e70ec8955d765bc6124e838
abdullahalnomanfb <abdullahalnoman1512@gamil.com>
2023-05-30 00:10:13 +0600

    add

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@
 import { useState } from "react";
 import { LoginSocialFacebook } from "reactjs-social-login";
 import { FacebookLoginButton } from "react-social-login-buttons";
+import axios from "axios";
 
 function App() {
   const [profile, setProfile] = useState(null);
+  const [accounts, setAccounts] = useState([])
+
+  console.log("accounts",accounts);
+  const getAccounts = async (token) => {
+    try {
+      const {data} = await axios.get(  `https://graph.facebook.com/me/accounts?access_token=${token}`);
+      setAccounts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
@@ -12,10 +24,12 @@ function App() {
         <LoginSocialFacebook
           // scope="name user_posts, email, read_insights, pages_manage_instant_articles, pages_show_list, read_page_mailboxes, ads_management, business_management, pages_messaging, instagram_basic, instagram_manage_comments, instagram_content_publish, publish_to_groups, instagram_manage_messages, page_events, pages_read_engagement, pages_manage_metadata"
           // redirect_uri="http://localhost:3000/account/login"
+          // appId="773303087736335"
           appId="773303087736335"
           onResolve={(response) => {
             console.log(response);
             setProfile(response.data);
+            getAccounts(response.data.accessToken);
           }}
           onReject={(error) => {
             console.log(error);
@@ -30,7 +44,9 @@ function App() {
       {profile ? (
         <div style={{ margin: "auto", width: "" }}>
           <h1>{profile.name}</h1>
-          <p style={{width:"100px",color:'red'}}>{`"${profile.accessToken}"`}</p>
+          <p
+            style={{ width: "100px", color: "red" }}
+          >{`"${profile.accessToken}"`}</p>
           <img
             style={{ widht: "100px", height: "100px", borderRadius: "100%" }}
             src={profile.picture.data.url}
@@ -38,6 +54,16 @@ function App() {
         </div>
       ) : (
         ""
+      )}
+
+      {accounts.length && (
+        <form action="">
+          <select name="" id="">
+            {accounts.data.map(({ name }) => (
+              <option value={name}>{name}</option>
+            ))}
+          </select>
+        </form>
       )}
     </div>
   );
