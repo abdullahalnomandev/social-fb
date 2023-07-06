418f3a1fbfd33d6964be8fe71596c92e7d71ffbb
Hp <abdullahalnoman1512.com>
2023-07-06 14:23:05 +0600

    add

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
     <div>
       {!profile ? (
         <LoginSocialFacebook
-          scope="name user_posts, email, read_insights, pages_manage_instant_articles, pages_show_list, read_page_mailboxes, ads_management, business_management, pages_messaging, instagram_basic, instagram_manage_comments, instagram_content_publish, publish_to_groups, instagram_manage_messages, page_events, pages_read_engagement, pages_manage_metadata"
+          // scope="name user_posts, email, read_insights, pages_manage_instant_articles, pages_show_list, read_page_mailboxes, ads_management, business_management, pages_messaging, instagram_basic, instagram_manage_comments, instagram_content_publish, publish_to_groups, instagram_manage_messages, page_events, pages_read_engagement, pages_manage_metadata"
           // redirect_uri="http://localhost:3000/account/login"
           // appId="773303087736335"
           appId="773303087736335"
